4076bb4c5a2eb48c074291b3dc2a72aa216b71fe
paulorjmx <pauloricardojm@hotmail.com.br>
2021-11-30 17:37:35 -0300

    Update exercise 1

--- respostas.sql
+++ respostas.sql
@@ -35,7 +35,40 @@ CREATE TABLE desfecho (
   FOREIGN KEY (id_paciente) REFERENCES pacientes(id) ON DELETE CASCADE
 );
 
-# Ao importar os dados do bpsp_desfecho_01.csv, verificou-se que o paciente abaixo, não tinha registro na tabela de 'pacientes'
-# Verifiquemos, então, se o mesmo possui exames e mais desfechos:
-SELECT * FROM desfecho WHERE id_paciente = '9F161F1AFB4D6041';
-SELECT * FROM exames WHERE id_paciente = '9F161F1AFB4D6041';
+SELECT * FROM pacientes LIMIT 10;
+
+SELECT * FROM exames LIMIT 10;
+
+SELECT * FROM desfecho LIMIT 10;
+
+# Vejamos quantos pacientes há na base de dados:
+SELECT COUNT(id) as qtd_pacientes FROM pacientes;
+
+# Vejamos quantos destes pacientes realizaram algum exame:
+SELECT COUNT(DISTINCT(id_paciente)) as qtd_pacientes_com_algum_exame FROM exames;
+
+# Verifiquemos qual paciente não possui correspondência na tabela exames
+SELECT pacientes.id FROM pacientes
+  LEFT JOIN exames ON exames.id_paciente = pacientes.id
+      WHERE exames.id_paciente IS NULL;
+
+# Verificando se há alguma desfecho do paciente acima
+SELECT * FROM desfecho WHERE desfecho.id_paciente = '9F161F1AFB4D6041';
+
+# Removendo o paciente
+DELETE FROM pacientes WHERE id = '9F161F1AFB4D6041';
+
+
+
+############# EXERCICIO 2 ##############
+# Qual a quantidade de pacientes presente na base de dados?
+SELECT COUNT(id) qtd_pacientes FROM pacientes;
+
+# Quantos são homens e quantos são mulheres?
+SELECT sexo, COUNT(sexo) as qtd_total FROM pacientes 
+  WHERE sexo = 'F' GROUP BY sexo
+UNION
+SELECT sexo, COUNT(sexo) as qtd_total FROM pacientes 
+  WHERE sexo = 'M' GROUP BY sexo;
+
+# Qual é faixa etária dos pacientes homens e mulheres?
