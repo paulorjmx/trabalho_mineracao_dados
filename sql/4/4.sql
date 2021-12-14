4d792b235889a7b2391981f49f535bb149e0af57
paulorjmx <pauloricardojm@hotmail.com.br>
2021-12-14 18:13:49 -0300

    Fix execise 4 queries

--- sql/4/4.sql
+++ sql/4/4.sql
@@ -1,23 +1,50 @@
 --Com subquery
-select id,p.municipio,maximo,minimo,idade from pacientes_idade p,
-(select max(idade) as maximo,min(idade) as minimo,municipio from  pacientes_idade group by municipio) mm
-where (p.municipio = mm.municipio and p.idade = mm.minimo) or p.municipio = mm.municipio and p.idade = mm.maximo
-order by p.municipio
+SELECT 
+	P.id, 
+    EXTRACT(YEAR FROM NOW()) - P.ano_nascimento as idade,
+    P.municipio, 
+    MM.IdMin, 
+    MM.idMax
+FROM pacientes P, (
+	SELECT 
+    	municipio, 
+      	Min(EXTRACT(YEAR FROM NOW()) - ano_nascimento) IdMin, 
+      	Max(EXTRACT(YEAR FROM NOW()) - ano_nascimento) IdMax
+FROM pacientes
+	GROUP BY municipio) MM
+		WHERE 
+        	(P.municipio=MM.municipio AND EXTRACT(YEAR FROM NOW()) - P.ano_nascimento=MM.IdMin) OR
+			(P.municipio=MM.municipio AND EXTRACT(YEAR FROM NOW()) - P.ano_nascimento=MM.IdMax)
+ORDER BY municipio ASC;
+
+
 
 --Com CTE
-
-with maximoMinimo as (select max(idade) as maximo,min(idade) as minimo,municipio from  pacientes_idade group by municipio)
-select id,p.municipio,maximo,minimo,idade from pacientes_idade p
-inner join maximoMinimo mm on 
-	((p.municipio = mm.municipio and p.idade = mm.minimo) or p.municipio = mm.municipio and p.idade = mm.maximo)
-order by p.municipio
-
---com window function
-
+with maximoMinimo as 
+	(select 
+     	max(EXTRACT(YEAR FROM NOW()) - ano_nascimento) as idade_maxima,
+     	min(EXTRACT(YEAR FROM NOW()) - ano_nascimento) as idade_minima,
+     	municipio 
+     FROM pacientes group by municipio)
 select 
 	id,
-	idade,
+    EXTRACT(YEAR FROM NOW()) - ano_nascimento as idade,
+    p.municipio,
+    idade_minima,
+    idade_maxima
+from pacientes p
+inner join maximoMinimo mm on 
+	((p.municipio = mm.municipio and EXTRACT(YEAR FROM NOW()) - ano_nascimento = mm.idade_minima) or 
+     p.municipio = mm.municipio and EXTRACT(YEAR FROM NOW()) - ano_nascimento = mm.idade_maxima)
+ORDER BY municipio ASC
+LIMIT 10;
+
+
+--com window function
+select 
+	id,
+	EXTRACT(YEAR FROM NOW()) - ano_nascimento as idade,
 	municipio,
-	max(idade) over(partition by municipio) as maximo,
-	min(idade) over(partition by municipio) as minimo
-from pacientes_idade
+	max(EXTRACT(YEAR FROM NOW()) - ano_nascimento) over(partition by municipio) as maximo,
+	min(EXTRACT(YEAR FROM NOW()) - ano_nascimento) over(partition by municipio) as minimo
+from pacientes LIMIT 10
