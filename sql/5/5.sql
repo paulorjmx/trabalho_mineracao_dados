8de3dbcca57a68e7709a6f129645bc27ecea781c
George Gantus <georgegantus@usp.br>
2021-12-14 16:06:47 -0300

    5a e 6

---
+++ sql/5/5.sql
@@ -0,0 +1,2 @@
+--a 
+select distinct descricao_analito from exames where upper(descricao_exame) like '%HEMOGRAMA%'
---
+++ sql/6/6.sql
@@ -0,0 +1,15 @@
+SELECT distinct
+    e.id_paciente,
+    e.id_atendimento,
+	d.descricao_desfecho,
+    AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'COLESTEROL NAO-HDL, SORO') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA,
+    AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'HDL-COLESTEROL') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_HDL,
+    AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'LDL COLESTEROL') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_LDL,
+    AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) LIKE ANY (ARRAY['VLDL-COLESTEROL','V-COLESTEROL'])) OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_VLDL,
+    AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'COLESTEROL TOTAL') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_COLESTEROL_TOTAL
+    --DESF.DAT_ATD,
+    --TIP_DESF
+FROM exames e
+inner join desfecho d on e.id_atendimento = d.id_atendimento
+where upper(descricao_exame) like '%COLESTEROL%' AND
+    (upper(resultado) ~* '[A-Z]') is false
