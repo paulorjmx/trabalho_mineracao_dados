cfc39d064738c30a956acf5e61cd055d5bcd12b6
George Gantus <georgegantus@usp.br>
2021-12-14 16:07:12 -0300

    5a e 6 pt2

--- sql/6/6.sql
+++ sql/6/6.sql
@@ -7,8 +7,6 @@ SELECT distinct
     AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'LDL COLESTEROL') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_LDL,
     AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) LIKE ANY (ARRAY['VLDL-COLESTEROL','V-COLESTEROL'])) OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_VLDL,
     AVG(cast(resultado as float)) FILTER (WHERE upper(descricao_analito) = 'COLESTEROL TOTAL') OVER (PARTITION BY e.id_paciente,e.id_atendimento) AS MEDIA_COLESTEROL_TOTAL
-    --DESF.DAT_ATD,
-    --TIP_DESF
 FROM exames e
 inner join desfecho d on e.id_atendimento = d.id_atendimento
 where upper(descricao_exame) like '%COLESTEROL%' AND
